80dfbec844eba5c0598d9afbc4ed474ffc750902
VaL <vd@nxc.no>
2013-05-28 12:37:43 +0200

    - Removed unneeded case

--- tests/nxcSerenataTemplateTest.php
+++ tests/nxcSerenataTemplateTest.php
@@ -19,29 +19,6 @@ class nxcSerenataTemplateTest extends PHPUnit_Framework_TestCase
         $e = file_get_contents( $_SERVER['PWD'] . '/extension/nxc_serenata/tests/files/s.eml' );
         $this->assertEquals( $e, $c );
     }
-
-    public function testSendConfirmationEML()
-    {
-        $t = new nxcSerenataTemplate( 'CONFIRMATION_DE' );
-        $t->setHeader( 'user', 'SERDEMO' );
-        $t->set( 'GUEST_LONG_TITLE', 'Dr. VaL' );
-        $t->set( 'GUEST_LAST_NAME', 'VaL' );
-        $t->set( 'ARRIVAL_DATE', '01-07-2013' );
-        $t->set( 'arrival_date_long_format', '01-07-2013' );
-        $t->set( 'DEPARTURE_DATE', '02-07-2013' );
-        $t->set( 'departure_date_long_format', '02-07-2013' );
-        $t->set( 'NO_OF_ADULTS', '1' );
-        $t->set( 'NO_OF_CHILDREN', '0' );
-        $t->set( 'ROOM_TYPE_DESCRIPTION', 'Super King Size Double' );
-        $t->set( 'ROOM_RATE', '150.00' );
-        $t->set( 'ROOM_REVENUE', '150' );
-        $t->set( 'RESERVATION_CLERK', 'my Fidelio' );
-        $t->set( 'RESV_NAME_ID', '1111' );
-        $t->set( 'CONFIRMATION_NO', '2222' );
-
-        $c = $t->send( 'vd@nxc.no' );
-    }
-
 }
 
 ?>
